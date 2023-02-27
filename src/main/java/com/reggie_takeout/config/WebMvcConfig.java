0f50a412ee42296d0bed3e0da221675c80817ef1
Waterdance1002 <waterdance1002@gmail.com>
2023-02-27 12:20:57 +0800

    employee logout

--- src/main/java/com/reggie_takeout/config/WebMvcConfig.java
+++ src/main/java/com/reggie_takeout/config/WebMvcConfig.java
@@ -9,9 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("开始静态资源映射...");
+        log.info("开始进行静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
--- src/main/java/com/reggie_takeout/controller/EmployeeController.java
+++ src/main/java/com/reggie_takeout/controller/EmployeeController.java
@@ -23,33 +23,52 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     /**
-     * employee login
+     * 员工登录
      * @param request
      * @param employee
      * @return
      */
-    @PostMapping("login")
-    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+
+        //1、将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
         password = DigestUtils.md5DigestAsHex(password.getBytes());
 
+        //2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
-        if(emp == null) {
-            return R.error("用户名不存在!");
+        //3、如果没有查询到则返回登录失败结果
+        if(emp == null){
+            return R.error("登录失败");
         }
 
-        if(!emp.getPassword().equals(password)) {
-            return R.error("密码错误!");
+        //4、密码比对，如果不一致则返回登录失败结果
+        if(!emp.getPassword().equals(password)){
+            return R.error("登录失败");
         }
 
-        if(emp.getStatus().equals(0)) {
-            return R.error("账号已禁用!");
+        //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if(emp.getStatus() == 0){
+            return R.error("账号已禁用");
         }
 
-        request.getSession().setAttribute("employee", emp.getId());
-        return R.success(employee);
+        //6、登录成功，将员工id存入Session并返回登录成功结果
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+    }
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
     }
 }
