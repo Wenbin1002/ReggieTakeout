f4eaa98e0c4f5ae221bbebd5d80f32ec28ea9dcf
Waterdance1002 <waterdance1002@gmail.com>
2023-03-04 10:25:04 +0800

    add dish function

--- src/main/java/com/reggie_takeout/controller/DishController.java
+++ src/main/java/com/reggie_takeout/controller/DishController.java
@@ -4,13 +4,12 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie_takeout.common.R;
 import com.reggie_takeout.entity.Dish;
+import com.reggie_takeout.entity.Employee;
 import com.reggie_takeout.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -21,11 +20,12 @@ public class DishController {
     private DishService dishService;
 
     @GetMapping("/page")
-    public R<Page<Dish>> pageR(int page, int pageSize) {
+    public R<Page<Dish>> pageR(int page, int pageSize, String name) {
 
         Page<Dish> pageInfo = new Page<>(page, pageSize);
 
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotEmpty(name), Dish::getName, name);
         queryWrapper.orderByDesc(Dish::getUpdateTime);
 
         dishService.page(pageInfo,queryWrapper);
@@ -33,11 +33,11 @@ public class DishController {
         return R.success(pageInfo);
     }
 
-//    @PostMapping
-//    public R<String> save(Dish dish) {
-//        log.info(dish.toString());
-//        //dishService.save(dish);
-//
-//        return R.success("添加菜品成功");
-//    }
+    @PostMapping
+    public R<String> save(@RequestBody Dish dish) {
+        log.info(dish.toString());
+        dishService.save(dish);
+
+        return R.success("添加菜品成功");
+    }
 }
